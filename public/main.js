4a2b66ee83433565e67e1cff3d1e2fd5d2ae242c
Gerni Oster <gernio@yahoo.com>
2019-07-07 22:34:13 -0400

    intro-to-js

--- public/main.js
+++ public/main.js
@@ -1,7 +1,66 @@
+let counter = 0
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  console.log('Hello world')
 }
+const addOneToCount = () => {
+  console.log('add 1 to counter')
+  // get current value of the counter
+  // add 1 to counter
+  counter = counter + 1
+  console.log(counter)
+  //update html
+  //hey Dom, find the p, update the content
+  document.querySelector('p').textContent = counter
+}
+const subtractOneFromCounter = () => {
+  console.log('subtract 1 to counter')
+   // get current value of the counter
+  // add 1 to counter
+  counter = counter - 1
+  console.log(counter)
+  //update html
+  //hey Dom, find the p, update the content
+  document.querySelector('p').textContent = counter
+}
+
+
+const updateName = () => {
+  console.log('team name') 
+  //get the value of the text
+  const updateName = document.querySelector('.team-1-input').value
+  //update html
+  document.querySelector('h2').textContent = updateName
+
+  
+}
+
+
+// Find the buttons, when you are clicked, do this thing
+document
+.querySelector ('.team-1-add-1-button')
+.addEventListener('click', addOneToCount)
+// Find the buttons, when you are clicked, do this thing
+
+document
+.querySelector ('.team-2-add-1-button')
+.addEventListener('click', addOneToCount)
+
+document
+.querySelector('.team-1-subtract-1-button')
+.addEventListener('click', subtractOneFromCounter)
+
+document
+.querySelector('.team-2-subtract-1-button')
+.addEventListener('click', subtractOneFromCounter)
+
+//Find the buttons, when clicked do the thing
+document
+.querySelector('.update-team-1-name')
+.addEventListener('focus', updateName)
+
+document
+.querySelector('.update-team-2-name')
+.addEventListener('focus', updateName)
+
 
 document.addEventListener('DOMContentLoaded', main)
